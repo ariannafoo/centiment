0ed2f71a357cf1042183926952a88a19bd216a9b
Arianna Foo <fooar@sheridancollege.ca>
2025-04-15 00:33:22 -0400

    Initial Commit

---
+++ Centiment/CentimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentimentApp.swift
+//  Centiment
+//
+//  Created by Arianna Foo on 2025-04-15.
+//
+
+import SwiftUI
+
+@main
+struct CentimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
